28d64fbcff549271685d34594d1b25b41cbe5e17
astellon <astellon2525otastepper@gmail.com>
2019-03-26 21:54:27 +0900

    dispatch for bundle (test)

--- src/osc-crystal/server.cr
+++ src/osc-crystal/server.cr
@@ -3,23 +3,33 @@ require "socket"
 module OSC
   class Server
     def initialize(@socket : Socket)
-      @methods = Array(Tuple(String, (OSC::Message ->))).new
+      @running = false
+      @methods_for_message = Array(Tuple(String, (OSC::Message ->))).new
+      @methods_for_bundle = Array((OSC::Bundle ->)).new
     end
 
-    def dispatch(address : String, &block : OSC::Message ->)
-      @methods << {address, block}
+    def dispatch(address : String, &block : OSC::Message->)
+      @methods_for_message << {address, block}
+    end
+
+    def dispatch_bundle(&block : OSC::Bundle ->)
+      @methods_for_message << block
     end
 
     def run
+      @running = true
+
       spawn do
-        loop do
+        while @running
           str, ip = @socket.receive(512)
           if OSC::Util.bundle?(str)
             b = OSC::Bundle.new(str.bytes)
-            # TODO: write process for Bundle
+            @methods_for_bundle.each do |method|
+              method.call(b)
+            end
           else
             m = OSC::Message.new(str.bytes)
-            @methods.each do |address, method|
+            @methods_for_message.each do |address, method|
               if File.match?(m.address, address)
                 method.call(m)
               end
@@ -28,13 +38,17 @@ module OSC
         end
       end
     end
+
+    def stop
+      @running = @running ^ false
+    end
   end
 
   class Client
     def initialize(@socket : Socket)
     end
 
-    def send(m : OSC::Message)
+    def send(m : OSC::Message | OSC::Bundle)
       @socket.send m
     end
   end
